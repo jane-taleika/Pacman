5ac51710c75984107a22795799a0c0e350e959df
Kostyshina <i.vote.saxon@gmail.com>
2015-11-12 02:09:51 +0300

    Shooting sphere created

---
+++ Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacmanMove : MonoBehaviour {
+
+    public float speed;
+    public GameObject shell;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Let's go pacman");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = speed * Input.GetAxis("Horizontal") + transform.position.x;
+        if (xAxis != 0){
+            transform.position = new Vector3(xAxis, transform.position.y, transform.position.z);
+        }
+        //transform.Translate(new Vector3(xAxis, 0.0f, 0.0f));
+        if (Input.GetKeyDown("space")) { 
+            Shot();
+        }
+	}
+
+    void Shot() {
+        GameObject shootShell;
+        shootShell = Instantiate(shell, transform.position, Quaternion.identity) as GameObject;
+    }
+}
---
+++ Assets/Scripts/ShellMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShellMove : MonoBehaviour {
+
+    float lifeTime = 3;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("pew");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.name == "Shell(Clone)"){
+            transform.Translate(new Vector3(1.0f, 0.0f, 0.0f) * speed);
+            Destroy(gameObject, lifeTime);
+        }
+    }
+}
